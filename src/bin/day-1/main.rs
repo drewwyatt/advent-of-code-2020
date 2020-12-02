e23520212fbe9ed1db2d4eba0511b8c585a07611
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-01 21:53:04 -0500

    day-1 part-1

--- src/bin/day-1/main.rs
+++ src/bin/day-1/main.rs
@@ -1,9 +1,24 @@
-use aoc::lines_from_input;
+use aoc::read_input_for_day;
+use std::io::Result;
 
-fn main() {
-  let lines = lines_from_input("day-1/INPUT");
-  match lines {
-    Err(e) => println!("oh no {:?}", e),
-    Ok(v) => println!("{:?}", v.get(0))
+fn main() -> Result<()> {
+  let lines = read_input_for_day("day-1")?;
+  let mut product: Option<i32> = None;
+
+  for idx in 0..lines.len() {
+    let n1 = lines.get(idx).unwrap();
+    match product {
+      Some(_) => break,
+      None => for idx2 in idx..lines.len() {
+        let n2 = lines.get(idx2).unwrap();
+        if n1 + n2 == 2020 {
+          product = Some(n1 * n2);
+          break;
+        }
+      }
+    }
   }
+
+  println!("Answer: {}", product.unwrap());
+  Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use std::{
-  fs::File,
-  io::{self, BufRead, BufReader},
-  path::Path,
-};
+use std::fs;
 
-pub fn lines_from_input(day_and_file: &str) -> io::Result<Vec<String>> {
-  BufReader::new(File::open(Path::new("./src/bin/").join(day_and_file))?).lines().collect()
+pub fn read_input_for_day(day: &str) -> std::io::Result<Vec<i32>> {
+  let path = format!("./src/bin/{}/INPUT", day);
+  let contents = fs::read_to_string(path)?;
+
+  let values = contents
+    .lines()
+    .map(|l| l.parse().unwrap())
+    .collect::<Vec<i32>>();
+
+  Ok(values)
 }
