f96794126cfae420480aa3abf8615bab2ebd4e35
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-02 14:49:50 -0500

    get part from structopt

--- src/bin/day-1/main.rs
+++ src/bin/day-1/main.rs
@@ -1,9 +1,30 @@
 use aoc::read_input_for_day;
-use std::io::Result;
+use std::io;
+use structopt::StructOpt;
 
-fn main() -> Result<()> {
+#[derive(StructOpt)]
+#[structopt(
+  about = "Solutions for Advent of Code 2020",
+  author = "Drew Wyatt <drew.j.wyatt@gmail.com>",
+  name = "advent-of-code-2020"
+)]
+struct Cli {
+  #[structopt(
+    default_value = "1",
+    help = "Part 1 or 2",
+    short,
+    long
+  )]
+  part: i32,
+}
+
+
+fn main() -> io::Result<()> {
   let lines = read_input_for_day("day-1")?;
   let mut product: Option<i32> = None;
+
+  let args = Cli::from_args();
+  println!("part: {}", args.part);
 
   for idx in 0..lines.len() {
     let n1 = lines.get(idx).unwrap();
