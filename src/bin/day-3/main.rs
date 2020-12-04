5c6f6aa4c61e59b7a28039308ee426fc520d4156
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-03 19:12:53 -0500

    day-3 part-2

--- src/bin/day-3/main.rs
+++ src/bin/day-3/main.rs
@@ -38,14 +38,10 @@ impl FromStr for Row {
   }
 }
 
-fn main() -> Result<()> {
-  let input = aoc::read_input_for_day::<Row>("day-3")?;
-  let down = 1;
-  let right = 3;
+fn trees_for_slope(input: &Vec<Row>, right: usize, down: usize) -> i64 {
   let mut next_row_index = down;
   let mut cell_index = right;
-
-  let number_of_trees = input.iter().enumerate().fold(0, |mut trees, (index, row)| {
+  input.iter().enumerate().fold(0, |mut trees, (index, row)| {
     if index == next_row_index {
       if row.is_tree(cell_index) {
         trees += 1;
@@ -55,9 +51,27 @@ fn main() -> Result<()> {
     }
 
     trees
-  });
+  })
+}
 
-  println!("The answer is: {}", number_of_trees);
+fn main() -> Result<()> {
+  let input = aoc::read_input_for_day::<Row>("day-3")?;
+  let part = aoc::read_part_for_day();
 
+  let answer = if part == 1 {
+    trees_for_slope(&input, 3, 1)
+  } else {
+    vec![
+      trees_for_slope(&input, 1, 1),
+      trees_for_slope(&input, 3, 1),
+      trees_for_slope(&input, 5, 1),
+      trees_for_slope(&input, 7, 1),
+      trees_for_slope(&input, 1, 2),
+    ]
+    .iter()
+    .fold(1, |acc, trees| acc * trees)
+  };
+
+  println!("The answer for part-{} is: {}", part, answer);
   Ok(())
 }
