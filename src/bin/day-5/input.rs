07674c8996983461e1e5344b337b358d56f86508
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-07 00:48:54 -0500

    more ownership problems in tree.value()?

--- src/bin/day-5/input.rs
+++ src/bin/day-5/input.rs
@@ -1,27 +1,101 @@
-use std::io;
 use std::str::FromStr;
 
+const NUMBER_OF_ROWS: i32 = 128;
+const NUMBER_OF_SEATS: i32 = 8;
+
 #[derive(Debug)]
-pub enum AdventError {}
+pub enum AdventError {
+  UnrecognizedChar,
+  InvalidTree,
+}
+
+#[derive(Debug)]
+enum RowLetter {
+  F,
+  B,
+}
 
 #[derive(Debug)]
 
+enum SeatLetter {
+  L,
+  R,
+}
+
+impl RowLetter {
+  fn from_char(c: char) -> Result<Self, AdventError> {
+    match c {
+      'F' => Ok(RowLetter::F),
+      'B' => Ok(RowLetter::B),
+      _ => Err(AdventError::UnrecognizedChar),
+    }
+  }
+}
+
+impl SeatLetter {
+  fn from_char(c: char) -> Result<Self, AdventError> {
+    match c {
+      'L' => Ok(SeatLetter::L),
+      'R' => Ok(SeatLetter::R),
+      _ => Err(AdventError::UnrecognizedChar),
+    }
+  }
+}
+
+#[derive(Debug)]
 pub struct BoardingPass {
-  row_code: String,
-  seat_code: String,
+  row_code: Vec<RowLetter>,
+  seat_code: Vec<SeatLetter>,
+}
+
+impl BoardingPass {
+  pub fn new(code: &str) -> Result<Self, AdventError> {
+    BoardingPass::from_str(code)
+  }
+
+  pub fn row(&self) -> Result<i32, AdventError> {
+    let mut tree = Tree::new(NUMBER_OF_ROWS);
+    for letter in self.row_code.iter() {
+      match letter {
+        RowLetter::F => tree.lower(),
+        RowLetter::B => tree.upper(),
+      };
+    }
+
+    tree.value()
+  }
+
+  pub fn seat(&self) -> Result<i32, AdventError> {
+    let mut tree = Tree::new(NUMBER_OF_SEATS);
+    for letter in self.seat_code.iter() {
+      println!("{:?}", tree);
+      match letter {
+        SeatLetter::L => tree.lower(),
+        SeatLetter::R => tree.upper(),
+      };
+    }
+
+    tree.value()
+  }
+
+  pub fn id(&self) -> Result<i32, AdventError> {
+    let row = self.row()?;
+    let seat = self.seat()?;
+    Ok(row * 8 + seat)
+  }
 }
 
 impl FromStr for BoardingPass {
   type Err = AdventError;
 
   fn from_str(line: &str) -> Result<Self, Self::Err> {
-    let mut row_code = String::from("");
-    let mut seat_code = String::from("");
+    let mut row_code = vec![];
+    let mut seat_code = vec![];
     for (index, char) in line.chars().enumerate() {
       if index < 7 {
-        row_code.push(char);
+        row_code.push(RowLetter::from_char(char)?);
       } else {
-        seat_code.push(char);
+        seat_code.push(SeatLetter::from_char(char)?);
       }
     }
 
@@ -41,15 +115,19 @@ struct Tree {
 }
 
 impl Tree {
+  fn new(size: i32) -> Self {
+    Tree { start: 0, size }
+  }
+
   fn done(&self) -> bool {
     self.size == 1
   }
 
-  fn value(&self) -> io::Result<i32> {
+  fn value(&self) -> Result<i32, AdventError> {
     if self.done() {
       Ok(self.size)
     } else {
-      Err(io::Error::new(io::ErrorKind::Other, "value not yet known"))
+      Err(AdventError::InvalidTree)
     }
   }
 
--- src/bin/day-5/main.rs
+++ src/bin/day-5/main.rs
@@ -9,7 +9,15 @@ use std::io::Result;
 // BBFFBBFRLL: row 102, column 4, seat ID 820.
 
 fn main() -> Result<()> {
-  let boarding_passes = read_input_for_day::<BoardingPass>("day-5")?;
-  println!("boarding pass: {:?}", boarding_passes.get(0));
+  // let boarding_passes = read_input_for_day::<BoardingPass>("day-5")?;
+  let bp = BoardingPass::new("BFFFBBFRRR").unwrap();
+  // println!("boarding pass: {:?}", boarding_passes.get(0));
+  println!("{:?}", bp);
+  println!(
+    "row: {:?}, seat: {:?}, id: {:?}",
+    bp.row(),
+    bp.seat(),
+    bp.id()
+  );
   Ok(())
 }
