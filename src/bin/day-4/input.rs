1e840f3faa41e135cefeb63766ec98e92038455a
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-12-06 22:25:58 -0500

    use hashset instead of vec

--- src/bin/day-4/input.rs
+++ src/bin/day-4/input.rs
@@ -1,20 +1,50 @@
 use regex::{Captures, Regex};
+use std::collections::HashSet;
 use std::io::{Error, ErrorKind, Result};
 
 #[derive(Debug)]
 pub struct Document {
-  fields: Vec<Field>,
+  fields: HashSet<Field>,
 }
 
 impl Document {
-  fn push(&mut self, field: Field) {
-    self.fields.push(field);
+  fn new() -> Self {
+    Document {
+      fields: HashSet::new(),
+    }
+  }
+
+  fn add(&mut self, capture: &Captures) -> Result<()> {
+    let name = self.field_from_str(capture.get(1).unwrap().as_str());
+    match name {
+      Some(field) => {
+        self.fields.insert(field);
+        Ok(())
+      }
+      None => Err(Error::new(
+        ErrorKind::Other,
+        format!("Error parsing input '{:?}'", capture),
+      )),
+    }
+  }
+
+  fn field_from_str(&self, s: &str) -> Option<Field> {
+    match s {
+      "byr" => Some(Field::BirthYear),
+      "iyr" => Some(Field::IssueYear),
+      "eyr" => Some(Field::ExpirationYear),
+      "hgt" => Some(Field::Height),
+      "hcl" => Some(Field::HairColor),
+      "ecl" => Some(Field::EyeColor),
+      "pid" => Some(Field::PassportId),
+      "cid" => Some(Field::CountryId),
+      _ => None,
+    }
   }
 }
 
-#[derive(Debug)]
-
-pub enum FieldName {
+#[derive(Debug, Eq, Hash, PartialEq)]
+pub enum Field {
   BirthYear,
   IssueYear,
   ExpirationYear,
@@ -25,56 +55,22 @@ pub enum FieldName {
   CountryId, // We don't care about this
 }
 
-#[derive(Debug)]
-pub struct Field {
-  name: FieldName,
-}
-
-impl Field {
-  fn from(capture: &Captures) -> Result<Self> {
-    let name = Field::name_from_str(capture.get(1).unwrap().as_str());
-    match name {
-      Some(n) => Ok(Field { name: n }),
-      None => Err(Error::new(
-        ErrorKind::Other,
-        format!("Error parsing input '{:?}'", capture),
-      )),
+pub fn documents_from_input(input: Vec<String>) -> Result<Vec<Document>> {
+  let documents = input.iter().fold(vec![Document::new()], |mut docs, line| {
+    lazy_static! {
+      static ref RE: Regex = Regex::new(r"([a-z]{3}):").unwrap();
     }
-  }
 
-  fn name_from_str(s: &str) -> Option<FieldName> {
-    match s {
-      "byr" => Some(FieldName::BirthYear),
-      "iyr" => Some(FieldName::IssueYear),
-      "eyr" => Some(FieldName::ExpirationYear),
-      "hgt" => Some(FieldName::Height),
-      "hcl" => Some(FieldName::HairColor),
-      "ecl" => Some(FieldName::EyeColor),
-      "pid" => Some(FieldName::PassportId),
-      "cid" => Some(FieldName::CountryId),
-      _ => None,
+    if RE.is_match(line) {
+      for cap in RE.captures_iter(line) {
+        let doc = docs.last_mut().unwrap();
+        doc.add(&cap);
+      }
+    } else {
+      docs.push(Document::new())
     }
-  }
-}
 
-pub fn documents_from_input(mut input: Vec<String>) -> Result<Vec<Document>> {
-  let documents = input
-    .iter()
-    .fold(vec![Document { fields: vec![] }], |mut docs, line| {
-      lazy_static! {
-        static ref RE: Regex = Regex::new(r"([a-z]{3}):").unwrap();
-      }
-
-      if RE.is_match(line) {
-        for cap in RE.captures_iter(line) {
-          let doc = docs.last_mut().unwrap();
-          doc.push(Field::from(&cap).unwrap())
-        }
-      } else {
-        docs.push(Document { fields: vec![] })
-      }
-
-      docs
-    });
+    docs
+  });
   Ok(documents)
 }
